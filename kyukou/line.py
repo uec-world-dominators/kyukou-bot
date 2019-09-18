2d2f778419a3c18506796c162586e351729209af
sho <shosatojp2001@gmail.com>
2019-09-18 13:10:31 +0900

    update line api helper functions

--- kyukou/line.py
+++ kyukou/line.py
@@ -1,28 +1,15 @@
-
-import requests
-from .settings import settings
-import hmac
-import hashlib
-import base64
-
-# Webhookから来たリクエストの署名の検証
-def validate(environ, body):
-    xsignature = environ["HTTP_X_LINE_SIGNATURE"].encode('utf-8')
-    channel_secret = settings["line"]["channel_secret"].encode('utf-8')
-    digest = hmac.new(channel_secret, body, hashlib.sha256).digest()
-    base64_digest = base64.b64encode(digest)
-    return xsignature == base64_digest
+from . import line_api
 
 
-def reply(data):
-    url = 'https://api.line.me/v2/bot/message/reply'
-    requests.post(url, json={
-        "replyToken": data["events"][0]["replyToken"],
-        "messages": [{
-            'type': 'text',
-            'text': data["events"][0]["message"]["text"] + 'ほげほげ',
-        }]
-    }, headers={
-        'content-type': 'application/json',
-        'authorization': f'Bearer {settings["line"]["access_token"]}'
-    })
+def follow(user_id):
+    print(f'followed by {user_id}')
+    line_api.reply(user_id, 'こんにちは')
+
+
+def unfollow(user_id):
+    print(f'unfollowed by {user_id}')
+
+
+def message(user_id, msg_text):
+    print(f'message from {user_id}: {msg_text}')
+    line_api.reply(user_id, msg_text)
