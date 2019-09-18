21bd7b3b5d888eee9dc71c17ab5bffaf405f677f
sho <shosatojp2001@gmail.com>
2019-09-18 17:00:38 +0900

    update database initiation

--- kyukou/db.py
+++ kyukou/db.py
@@ -7,10 +7,14 @@ class Db:
     @classmethod
     def init(cls, url):
         cls._client = MongoClient(url)
-        print(f'connected to mongo db with url {url} .')
+        print(f'Connected to DB: "{url}"')
         cls._db = cls._client.kyukou
         cls._users = cls._db.users
         cls._lectures = cls._db.lectures
+
+        print(f'----------------------------')
+        print(f'Number of Users: {cls._users.count()}')
+        print(f'----------------------------')
 
     @classmethod
     def get_users_db(cls):
