c791351058d86a3090d715f63d5615fd6dc749a1
sho <shosatojp2001@gmail.com>
2019-09-18 23:18:36 +0900

    update endpoints

--- kyukou/routes.py
+++ kyukou/routes.py
@@ -7,24 +7,24 @@ import re
 # 上から順に優先
 
 # LINE botからイベントがあったときに来る
-@route('post', '/api/line/webhook')
+@route('post', '/api/v1/line/webhook')
 def line_webhook(environ):
     body = get_body(environ)
     o = body_to_json(body)
     if line_api.validate(environ, body):
-        # print(body_to_utf8(body))
         line_api.parse(o)
         return status(200)
     else:
         return status(403)
 
 
-@route('post', '/api/upload')
+@route('post', '/api/v1/upload')
 def upload_csv(environ):
     print(get_body(environ))
+    return status(200)
 
 
-@route('post', '/api/email/register')
+@route('post', '/api/v1/email/register')
 def email(environ):
     email_api.register(get_body_json(environ))
     return status(200)
