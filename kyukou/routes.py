98a47fa3bb7d87b6334674c8da15490bd5e2e986
sho <shosatojp2001@gmail.com>
2019-09-18 22:08:55 +0900

    impliment file server

--- kyukou/routes.py
+++ kyukou/routes.py
@@ -1,6 +1,7 @@
 from .route import *
 from pprint import pprint
 from . import line_api
+from . import email_api
 import re
 
 # 上から順に優先
@@ -11,13 +12,24 @@ def line_webhook(environ):
     body = get_body(environ)
     o = body_to_json(body)
     if line_api.validate(environ, body):
-        print(body_to_utf8(body))
+        # print(body_to_utf8(body))
         line_api.parse(o)
         return status(200)
     else:
         return status(403)
 
 
+@route('post', '/api/upload')
+def upload_csv(environ):
+    print(get_body(environ))
+
+
+@route('post', '/api/email/register')
+def email(environ):
+    email_api.register(get_body_json(environ))
+    return status(200)
+
+
 @route(re.compile('.*'), '/')
 def fallback(environ):
-    return text('This is fallback')
+    return file(environ["PATH_INFO"])
