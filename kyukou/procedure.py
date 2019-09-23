c1cce1930923ae5549b2c6db3e5233faa4799a33
shosatojp <shosatojp2001@gmail.com>
2019-09-23 17:24:51 +0900

    update Procedure for DB

--- kyukou/procedure.py
+++ kyukou/procedure.py
@@ -1,3 +1,7 @@
+import time
+from .db import get_collection
+import random
+from pprint import pprint
 
 
 def process(p, n):
@@ -46,8 +50,35 @@ class Procedure():
         self.set_progress(id, -1)
 
 
+class ProcedureDB(Procedure):
+    def __init__(self, oncondition, timeout=3600):
+        self.collection = get_collection('procedure')
+        self.processes = []
+        self.oncondition = oncondition
+        self.procedure_id = random.random()
+        self.timeout = timeout
+        self.clear()
+
+    def set_progress(self, id, progress):
+        if not self.collection.update_one({'id': id, 'procedure_id': self.procedure_id}, {
+                '$set': {
+                    'progress': progress
+                }}).matched_count:
+            self.collection.insert_one({'id': id, 'procedure_id': self.procedure_id,
+                                        'progress': progress,
+                                        'expired_at': time.time()+self.timeout})
+
+    def get_progress(self, id):
+        r = self.collection.find_one({'id': id, 'procedure_id': self.procedure_id})
+        return -1 if r == None else r.get('progress')
+
+    def clear(self):
+        self.collection.delete_many({'procedure_id': self.procedure_id,
+                                     'expired_at': {'$lt': time.time()}})
+
+
 class ProcedureSelector():
-    def __init__(self, procedures):
+    def __init__(self, *procedures):
         self.procedures = procedures
         self.current = {}
 
@@ -65,9 +96,14 @@ class ProcedureSelector():
         for procedure in self.procedures:
             procedure.end(id)
 
+    def clear(self):
+        for procedure in self.procedures:
+            if hasattr(procedure, 'clear'):
+                procedure.clear()
 
-if __name__ == '__main__':
-    p = Procedure(lambda id, *args: args[0] == 'mail')
+
+if __name__ == '__main__' or True:
+    p = ProcedureDB(lambda id, *args: args[0] == 'mail')
     @process(p, 0)
     def process0(id, args):
         print('0', id)
@@ -78,14 +114,14 @@ if __name__ == '__main__':
         print('1', id)
         p.set_progress(id, 1)
 
-    p2 = Procedure(lambda id, *args: args[0] == 'csv')
+    p2 = ProcedureDB(lambda id, *args: args[0] == 'csv')
 
     @process(p2, 0)
     def process20(id, args):
         print('p2', id)
         p2.set_progress(id, 0)
 
-    ps = ProcedureSelector([p, p2])
+    ps = ProcedureSelector(p, p2)
     args = ['mail']
     ps.run('hoge', *args)
     ps.run('hige', *args)
@@ -94,4 +130,4 @@ if __name__ == '__main__':
     args = ['me@shosato.jp']
     ps.run('hoge', *args)
 
-__all__ = ["Procedure", "process", "ProcedureSelector"]
+__all__ = ["Procedure", "ProcedureDB", "process", "ProcedureSelector"]
