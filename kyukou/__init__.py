d7c4cf3dd8aabf89875487fa4c35a805d10d1af6
shosato <me@shosato.jp>
2019-09-25 23:29:53 +0900

    update

--- kyukou/__init__.py
+++ kyukou/__init__.py
@@ -1,9 +1,11 @@
+import sys
 from .settings import *
 load_settings('config.yml')
 settings=get_settings()
 
+
 from .util import log
-log(__name__,f'Kyukou Bot started at: "{settings.url_prefix()}"')
+log(__name__,f'Kyukou Bot started at: "{settings.url_prefix()}"',5)
 
 from . import db
 db.init(settings.mongo_url())
@@ -12,9 +14,7 @@ from .server import run_server
 from .routes import *
 from . import scryping
 
-import sys
 sys.stdout.flush()
-
 
 from . import scheduler
 scheduler.init(tick_interval_sec=1)
