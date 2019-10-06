786c4f34178000c0f03ea6deeac0f189e9df432f
chop_me <ryo.nakai0@gmail.com>
2019-10-06 10:58:38 +0900

    通知時間の設定について、UI部分をほぼ完成、デバッグ未完

--- kyukou/line.py
+++ kyukou/line.py
@@ -81,33 +81,70 @@ if isinpackage:
 else:
     time_procedure = Procedure(lambda user_id, msg_text: msg_text == 'time', 'time')
 
-
 @process(time_procedure, 0)
-def please_enter_day(user_id, msg_text):
-    line_api.reply(user_id, ['休講の何日前に通知しますか？\n当日の場合は"0"、前日の場合は"1"、2日前の場合は"2"...のように入力してください。'])
+def add_notice(user_id, msg_text):
+    line_api.reply(user_id, ['通知時間を追加しますか？追加する場合は"y"、終了する場合は"end"と入力してください。'])
     time_procedure.set_progress(user_id, 0)
 
-
 @process(time_procedure, 1)
+def validate_input(user_id, msg_text): 
+    if msg_text == 'y':
+        time_procedure.set_progress(user_id, 1)
+    else:
+        line_api.reply(user_id, ['入力が間違っています。もう一度入力してください。'])
+        time_procedure.set_progress(user_id, 0)
+
+@process(time_procedure, 2)
+def select_notice_time(user_id, msg_text):
+    line_api.reply(user_id, ['通知の形式を選択し、対応する数字を入力してください。\n\
+        ・休講情報を見つけたら即時通知する: 1 \n\
+            ・休講日の何日前、何時間前など、通知時間を細かく設定する: 2\
+                尚、この通知設定は複数追加できます。'])
+    time_procedure.set_progress(user_id, 2)
+
+#process4かprocess11に分岐
+@process(time_procedure, 3)
+def validate_num(usr_id, msg_text):
+    if msg_text == '1':
+        time_procedure.set_progress(user_id, 10)
+    elif msg_text == '2':
+        time_procedure.set_progress(user_id, 3)
+    else:
+        line_api.reply(user_id, ['数値の形式が間違っています。もう一度入力してください。なお、全角数字は対応しておりません。'])
+        time_procedure.set_progress(user_id, 2)
+
+@process(time_procedure, 4)
+def please_enter_day(user_id, msg_text):
+    line_api.reply(user_id, ['休講の何日前に通知しますか？\n当日の場合は"0"、前日の場合は"1"、2日前の場合は"2"...のように入力してください。'])
+    time_procedure.set_progress(user_id, 4)
+
+@process(time_procedure, 5)
 def please_enter_time(user_id, msg_text):
     if re.match("\d*", msg_text):
         line_api.reply(user_id, ['その日の何時に通知しますか？\n"6:30"、"23:00"のように"時:分"となるよう24時間表記で送信してください。'])
         time_procedure.set_info(user_id, 'day', msg_text)
-        time_procedure.set_progress(user_id, 1)
+        time_procedure.set_progress(user_id, 5)
     else:
         line_api.reply(user_id, ['数値の形式が間違っています。もう一度入力してください。なお、全角数字は対応しておりません。'])
-        email_procedure.set_progress(user_id, 0)
+        email_procedure.set_progress(user_id, 4)
 
-
-@process(time_procedure, 2)
+@process(time_procedure, 6)
 def validate_time(user_id, msg_text):
     try:
         time_data = datetime.strptime(msg_text, '%H:%M')
         line_api.reply(user_id, ['通知時間を登録しました。'])
-        time_procedure.set_progress(user_id, 2)
+        time_procedure.set_progress(user_id, 6)
+    except ValueError:
+        line_api.reply(user_id, ['数値の形式が間違っています。もう一度入力してください。なお、全角数字は対応しておりません。'])
+        time_procedure.set_progress(user_id, 5)
     except:
-        line_api.reply(user_id, ['数値の形式が間違っています。もう一度入力してください。'])
-        time_procedure.set_progress(user_id, 1)
+        line_api.reply(user_id, ['不明なエラーが発生しました。申し訳ありませんが、最初からやり直してください。'])
+        ps.end(user_id)
+
+#分岐先のprocess11
+@process(time_procedure, 11)
+def kyuko_soku_tsuchi(user_id, msg_text):
+    line_api.reply(user_id, ['登録完了です。休講情報を見つけたらすぐ通知します。'])
 
 
 def message(user_id, msg_text):
